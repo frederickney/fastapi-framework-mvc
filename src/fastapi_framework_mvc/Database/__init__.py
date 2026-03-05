1c0e8e82847184c5f9eb5ee4a3bf586d430f38bb
frederick <frederick.ney@gmail.com>
2026-03-05 18:29:02 +0100

    add safe directly to database modeule for forward compatibility to 1.3.0

--- src/fastapi_framework_mvc/Database/__init__.py
+++ src/fastapi_framework_mvc/Database/__init__.py
@@ -7,3 +7,4 @@ __author__ = 'Frederick NEY'
 from fastapi_framework_mvc.Deprecation import module_deprecation
 module_deprecation(__name__, __name__.lower(), '1.3.0')
 from .driver import Driver as Database
+from .decorators import safe
